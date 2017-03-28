9495413fd5a8e6aa35f47fb905b1ce4a58276bcd
Nick Rycar <rycar@chef.io>
2017-03-28 15:22:22 -0700

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/recipes/provision.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: provision
+#
+# Copyright:: 2017, Nick Rycar, All Rights Reserved.
+include_recipe 'delivery-truck::provision'
---
+++ .delivery/build_cookbook/recipes/publish.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: publish
+#
+# Copyright:: 2017, Nick Rycar, All Rights Reserved.
+include_recipe 'delivery-truck::publish'
